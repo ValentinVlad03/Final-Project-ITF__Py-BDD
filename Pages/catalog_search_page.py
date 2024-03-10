6b87832800300ec6efe71dada0192d031eabebc7
ValentinVlad03 <valentinvlad03@gmail.com>
2024-03-10 12:11:25 +0200

    updated comments on methods

--- Pages/catalog_search_page.py
+++ Pages/catalog_search_page.py
@@ -19,14 +19,15 @@ class CatalogSearch_page(Browser):
     MISSING_PRODUCT_MESSAGE = (By.XPATH,'//div[@class="missing-records__sub-message"]')
     #  selectorul pentru titlul căutarii afişat în partea stângă sus a grilei cu produsele afişate
     RESULTS_PAGE_TITLE = (By.XPATH,'//h1[@class="page__heading"]')
-    # selectorii pentru casuta cu optiunile de sortare, cel general si cel pentru pret ascendent
+
+    # selectorii pentru căsuţa cu opţiunile de sortare, cel general şi cel pentru pret_ascendent
     SORT_VIEW_LABEL = (By.ID, 'sort_options')
     SORT_PRICE_ASCENDING_LABEL = (By.XPATH, '//*[@id="sort_options"]//option[@ value="price_asc"]')
     FAVORITE_BUTTON = (By.XPATH, '//*[@id="main"]//*[@title="Adauga la Favorite"]')
     HEART_FAVORITE_LINK = (By.XPATH, '//*[@id="header_wrapper"]/div[3]/a[1]/div')
     COREL_PDF_PRODUCT = (By.XPATH, '//*[@id="main"]//a[@title ="Corel PDF Fusion ENG - Licenta permanenta"]')
 
-    # selectorii pentru a naviga catre pagina produsului "Corel PDF Fusion" pentru a-l scoate din lista de Favorite
+    # selectorii pentru a naviga către pagina produsului "Corel PDF Fusion" pentru a-l scoate din lista de Favorite
     SOFTWARE_CATEGORY_LINK = (By.XPATH, '//li[@id="header_menu_item_100009465"] //a[@href="/catalog/software-653469"]')
     COREL_CATEGORY_LINK = (By.XPATH, '//li[@class="facet__option facet__option--tree facet__option--level-2"] //a[@href="/catalog/corel-653419"]')
     COREL_PDF_FUSION_CATEGORY_LINK = (By.XPATH, '//li[@class="facet__option facet__option--tree facet__option--level-3"] //a[@href="/catalog/corel-pdf-fusion-653433"]')
@@ -34,13 +35,14 @@ class CatalogSearch_page(Browser):
     COREL_PDF_SAVE_WISHLIST_LINK = (By.XPATH, '//a[@class="product-summary__save-wishlist btn product-summary__save-wishlist--saved"]')
     EMPTY_LIST_FAV_LIST_MSG = (By.XPATH, '//h1[@class="wishlist__empty-title"]')
 
+
+
 # metode pentru scenariul T6
     def insert_invalid_product(self, invalid_product):
         text_to_search = self.chrome.find_element(*self.SEARCH_INLINE)
         text_to_search.send_keys(Keys.CONTROL, 'a')
         text_to_search.send_keys(Keys.BACKSPACE)
         text_to_search.send_keys(invalid_product)
-        sleep(3)
 
     def click_search_button(self):
         inquiry_square = self.chrome.find_element(*self.SEARCH_BUTTON)
@@ -48,9 +50,9 @@ class CatalogSearch_page(Browser):
 
     def no_result_msg(self, no_results):
         expected_message = no_results
-        visible_message = self.chrome.find_element(*self.MISSING_PRODUCT_MESSAGE).text  #
-        assert visible_message.strip() == expected_message.strip()  # ne folosim de functia stipe ca sa
-        # eliminam spatiile goala de la inceputul sau sfarsitul unui sir de caractere, in caz ca exista
+        visible_message = self.chrome.find_element(*self.MISSING_PRODUCT_MESSAGE).text
+        assert visible_message.strip() == expected_message.strip()  # ne folosim de functia strip ca să
+        # eliminăm spaţiile goale de la începutul sau sfârşitul unui şir de caractere, în caz că există.
         logging.info(f"Test passed => Missing records message is correctly shown :  {str(visible_message)}")
 
 
@@ -61,14 +63,13 @@ class CatalogSearch_page(Browser):
         text_to_search.send_keys(Keys.CONTROL, 'a')
         text_to_search.send_keys(Keys.BACKSPACE)
         text_to_search.send_keys(product_name)
-        sleep(3)
 
     def check_listed_products_page_label(self, results_title):
         title_of_page = self.chrome.find_element(*self.RESULTS_PAGE_TITLE)
         current_title = title_of_page.text
         assert current_title == results_title
         logging.info(f"Test passed => The title of page with the searching results is correctly shown :  {str(results_title)}")
-        sleep(3)
+
 
 
 # metode pentru scenariul T8
@@ -79,12 +80,14 @@ class CatalogSearch_page(Browser):
     def select_ascending_price_option(self):
         sorting_option = WebDriverWait(self.chrome, 10).until(
             EC.visibility_of_element_located(self.SORT_PRICE_ASCENDING_LABEL))
+        # am adaugat o pauză de aşteptare pentru a avea timp să se afişeze lista cu opţiuni
         sorting_option.click()
 
     def check_ascending_order_url(self):
         current_page = self.chrome.current_url
         text_to_find = "sort_by=price_asc"
         assert text_to_find in current_page, "The URL doesn't contain the sorting option (ascending price)."
+
 
 
 # metode pentru scenariul T9
@@ -95,7 +98,7 @@ class CatalogSearch_page(Browser):
         search_bar = self.chrome.find_element(*self.SEARCH_INLINE)
         search_bar.send_keys("Corel PDF Fusion ENG - Licenta permanenta")
         self.chrome.find_element(*self.SEARCH_BUTTON).click()
-        # am adaugat o pauza de asteptare pentru a avea timp sa incarce butonul de FAVORITE
+        # am adaugat o pauză de aşteptare pentru a avea timp să se încarce butonul de FAVORITE
         fav_button = WebDriverWait(self.chrome, 10).until(
             EC.visibility_of_element_located(self.FAVORITE_BUTTON))
         fav_button.click()
@@ -135,13 +138,7 @@ class CatalogSearch_page(Browser):
         save_to_wishlist.click()
 
     def check_if_empty_favorite_list(self):
-        sleep(3)
         expected_msg = "Nu ai adaugat inca nimic in Favorite"
         displayed_msg = self.chrome.find_element(*self.EMPTY_LIST_FAV_LIST_MSG).text
-        # if expected_msg in displayed_msg:
-        #     print(f"The product named 'Corel PDF' is not listed in 'Favorites' list. ")
-        # else:
-        #     print(f"The product was not removed from 'Favorites' list.")
-
         assert expected_msg == displayed_msg
         logging.info("Test passed => The product named 'Corel PDF' is not listed in the 'Favorites' list.")
